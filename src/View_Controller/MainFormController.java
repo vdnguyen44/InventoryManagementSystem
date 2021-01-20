309e4e1620492a4e785d27460336e15d66c602bd
Vincent Nguyen <vdnguyen44@gmail.com>
2021-01-20 05:04:54 -0800

    create forms to add and modify parts

--- src/View_Controller/MainFormController.java
+++ src/View_Controller/MainFormController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class MainScreenController {
+public class MainFormController {
 
     @FXML
     private TableView<?> partsTable;
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainForm.fxml"));
         primaryStage.setTitle("Inventory Management System");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();

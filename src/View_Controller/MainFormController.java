12ed9561a1504b561b4b71ebd6a4026952ff7c03
Vincent Nguyen <vdnguyen44@gmail.com>
2021-01-23 00:17:42 -0800

    resize main application window and added exit button function

--- src/View_Controller/MainFormController.java
+++ src/View_Controller/MainFormController.java
@@ -1,11 +1,17 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
 
 public class MainFormController {
+
+    @FXML
+    private TitledPane mainForm;
 
     @FXML
     private TableView<?> partsTable;
@@ -45,5 +51,51 @@ public class MainFormController {
 
     @FXML
     private Button exitButton;
+
+    @FXML
+    void addPartButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addProductButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePartButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteProductButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitButtonPressed(ActionEvent event) {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void modifyPartButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modifyProductButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchPartButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchProductButtonPressed(ActionEvent event) {
+
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainForm.fxml"));
         primaryStage.setTitle("Inventory Management System");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1270, 555));
         primaryStage.show();
     }
 

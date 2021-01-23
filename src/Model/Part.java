04a50f736ee1ed9b01010c687f92c918057b46a7
Vincent Nguyen <vdnguyen44@gmail.com>
2021-01-23 00:54:28 -0800

    updated part class with constructor

--- src/Model/Part.java
+++ src/Model/Part.java
@@ -4,10 +4,19 @@ public abstract class Part {
 
     protected int partID;
     protected String partName;
-    protected double partPrice = 0.0;
+    protected double partPrice;
     protected int partStock;
     protected int partMin;
     protected int partMax;
+
+    public Part(int partID, String partName, double partPrice, int partStock, int partMin, int partMax) {
+        this.partID = partID;
+        this.partName = partName;
+        this.partPrice = partPrice;
+        this.partStock = partStock;
+        this.partMin = partMin;
+        this.partMax = partMax;
+    }
 
     public int getPartID() {
         return partID;
